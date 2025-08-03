66e201ffb01e2e713a2b5617c7f319191433b756
Azim Hatami <azimhatami.dev@gmail.com>
2025-08-03 16:37:17 +0330

    Display available rooms on the client

--- socket/main.js
+++ socket/main.js
@@ -3,6 +3,7 @@ const NamespaceHandler = require('./namespace');
 
 module.exports = {
   socketHandler : (io) => {
-    new NamespaceHandler(io).initConnection()
+    new NamespaceHandler(io).initConnection(),
+    new NamespaceHandler(io).createNamespacesConnection()
   }
 }
