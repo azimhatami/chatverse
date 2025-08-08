8730346b62e7d53bff5b5651cd8eab41ab36de50
Azim Hatami <azimhatami.dev@gmail.com>
2025-08-08 14:13:05 +0330

    Implement user model

---
+++ modules/support/user.model.js
@@ -0,0 +1,12 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema({
+  fullName: { type: String, required: true },
+  userName: { type: String, required: true },
+  mobile: { type: String, required: true, unique: true },
+
+}, {
+  timestamps: true
+});
+
+module.exports = model('User', userSchema);
